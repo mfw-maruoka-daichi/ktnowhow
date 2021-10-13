288402dbbfd22533c6eb687ef8023741238db69c
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2021-10-13 13:46:19 +0900

    first commit

---
+++ src/main/kotlin/com/moneyforward/ktnowhow/KtnowhowApplication.kt
@@ -0,0 +1,11 @@
+package com.moneyforward.ktnowhow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtnowhowApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtnowhowApplication>(*args)
+}
---
+++ src/test/kotlin/com/moneyforward/ktnowhow/KtnowhowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moneyforward.ktnowhow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtnowhowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
