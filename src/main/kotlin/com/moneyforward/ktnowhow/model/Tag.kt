c7dd669de505e5017cdb2ab06977cfcf795bb70b
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2021-10-19 11:04:07 +0900

    TagInputを自前で定義する
    TagInputは自動生成されるが、Tagのコンストラクタ変数の変更によって意図せずTagInputも変わる可能性もあるので

--- src/main/kotlin/com/moneyforward/ktnowhow/model/Tag.kt
+++ src/main/kotlin/com/moneyforward/ktnowhow/model/Tag.kt
@@ -1,10 +1,23 @@
 package com.moneyforward.ktnowhow.model
 
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import com.expediagroup.graphql.generator.scalars.ID
 import com.expediagroup.graphql.server.operations.Query
 import org.springframework.stereotype.Component
 
 data class Tag(
+    val id: ID,
+    val name: String,
+) {
+
+    @GraphQLIgnore
+    fun asInput(
+        id: ID = this.id,
+        name: String = this.name
+    ): TagInput = TagInput(id, name)
+}
+
+data class TagInput(
     val id: ID,
     val name: String,
 )
