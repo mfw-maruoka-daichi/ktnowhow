f1f8880f3ac2f7166a7a75c389cf9a0a0afbefcd
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2021-10-26 18:16:17 +0900

    クラス名が変わるとGraphQLのID.valueが変わるので、GraphQLNameアノテーションから取得して比較する

--- src/main/kotlin/com/moneyforward/ktnowhow/graphql/extention/LongIdTypeFeature.kt
+++ src/main/kotlin/com/moneyforward/ktnowhow/graphql/extention/LongIdTypeFeature.kt
@@ -1,6 +1,7 @@
 package com.moneyforward.ktnowhow.graphql.extention
 
 import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
+import com.expediagroup.graphql.generator.annotations.GraphQLName
 import com.expediagroup.graphql.generator.scalars.ID
 import com.moneyforward.ktnowhow.graphql.type.LongIdType
 import kotlin.reflect.KClass
@@ -10,12 +11,18 @@ interface LongIdTypeFeature {
     private val idDelimiter: String
         get() = ":"
 
-    fun Long.toID(clazz: KClass<out LongIdType>): ID = ID("${clazz.simpleName}${idDelimiter}$this")
-    fun ID.getRawId(clazz: KClass<out LongIdType>): Long? = value.split(idDelimiter).let {
-        when {
-            it.size != 2 -> null
-            it.first() != clazz.simpleName -> null
-            else -> it.last().toLongOrNull()
+    fun Long.toID(clazz: KClass<out LongIdType>): ID = ID("${clazz.simpleName}$idDelimiter$this")
+
+    fun ID.getRawId(clazz: KClass<out LongIdType>): Long? {
+        val (prefix, rawId) = value.split(idDelimiter).let {
+            if (it.size != 2) return null
+            it.first() to it.last()
         }
+
+        val graphqlTypeName =
+            (clazz.annotations.find { it.annotationClass == GraphQLName::class } as? GraphQLName)?.value
+                ?: clazz.simpleName
+
+        return if (prefix != graphqlTypeName) null else rawId.toLongOrNull()
     }
 }
