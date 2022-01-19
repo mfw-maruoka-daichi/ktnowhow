e43c70f98fc881bda2520d3cad17d2ef71cabe4f
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2022-01-19 21:08:59 +0900

    多:多も同様で、結局外部キー定義したかったらreference()で定義しないとダメっぽい
    https://github.com/JetBrains/Exposed/issues/1141

--- src/main/kotlin/com/moneyforward/ktnowhow/db/table/KnowhowsTags.kt
+++ src/main/kotlin/com/moneyforward/ktnowhow/db/table/KnowhowsTags.kt
@@ -4,8 +4,18 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
 object KnowhowsTags : LongIdTable("knowhows_tags") {
-    val knowhowId = long("knowhow_id").index("idx_knowhow_id")
-        .references(Knowhows.id, onUpdate = ReferenceOption.CASCADE, onDelete = ReferenceOption.CASCADE)
-    val tagId = long("tag_id").index("idx_tag_id")
-        .references(Tags.id, onUpdate = ReferenceOption.CASCADE, onDelete = ReferenceOption.CASCADE)
+    val knowhowId =
+        reference(
+            "knowhow_id",
+            Knowhows.id,
+            onUpdate = ReferenceOption.CASCADE,
+            onDelete = ReferenceOption.CASCADE
+        ).index("idx_knowhow_id")
+
+    val tagId = reference(
+        "tag_id",
+        Tags.id,
+        onUpdate = ReferenceOption.CASCADE,
+        onDelete = ReferenceOption.CASCADE
+    ).index("idx_tag_id")
 }
