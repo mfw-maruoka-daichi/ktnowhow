26f011eea39a1cec1a3ba1cf3c846c5ffb3aaf28
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2022-01-19 15:30:14 +0900

    久しぶりに触るとGraphQL playgroundのURLわからなくなるのでデフォルトのものをログに吐いておく

--- src/main/kotlin/com/moneyforward/ktnowhow/KtnowhowApplication.kt
+++ src/main/kotlin/com/moneyforward/ktnowhow/KtnowhowApplication.kt
@@ -1,5 +1,6 @@
 package com.moneyforward.ktnowhow
 
+import org.apache.commons.logging.LogFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class KtnowhowApplication
 
 fun main(args: Array<String>) {
-	runApplication<KtnowhowApplication>(*args)
+    runApplication<KtnowhowApplication>(*args)
+    LogFactory.getLog(KtnowhowApplication::class.java)
+        .info("default GraphQL Playground URL: http://localhost:8080/graphql")
 }
