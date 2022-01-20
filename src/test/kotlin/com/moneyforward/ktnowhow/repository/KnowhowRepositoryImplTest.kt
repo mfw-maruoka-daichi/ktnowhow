bf48796a2ccf74e2c5600d970a6b9dd685be1f6f
Daichi Maruoka <maruoka.daichi@moneyforward.co.jp>
2022-01-20 18:23:45 +0900

    KnowhowRepositoryImplのテスト追加
    - 全件取得
    - 追加時に存在しないユーザを指定

--- src/test/kotlin/com/moneyforward/ktnowhow/repository/KnowhowRepositoryImplTest.kt
+++ src/test/kotlin/com/moneyforward/ktnowhow/repository/KnowhowRepositoryImplTest.kt
@@ -5,15 +5,17 @@ import com.moneyforward.ktnowhow.db.table.Knowhows
 import com.moneyforward.ktnowhow.db.table.KnowhowsTags
 import com.moneyforward.ktnowhow.db.table.Tags
 import com.moneyforward.ktnowhow.db.table.Users
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
+import org.jetbrains.exposed.dao.exceptions.EntityNotFoundException
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class KnowhowRepositoryImplTest : DescribeSpec() {
 
-    // todo ProjectConfigか何かでやる
     override fun beforeSpec(spec: Spec) {
         H2TestDatabase.connect() // todo ProjectConfigか何かでやる
         transaction {
@@ -42,25 +44,51 @@ class KnowhowRepositoryImplTest : DescribeSpec() {
     private val tagRepository = TagRepositoryImpl()
     private val knowhowRepository = KnowhowRepositoryImpl()
 
+    private val author by lazy { transaction { userRepository.createUser("user1") } }
+    private val tags by lazy { transaction { tagRepository.createTags(listOf("tag1", "tag2", "tag3")) } }
+
     init {
-        describe("add knowhow") {
-            it("success") {
-                val author = transaction { userRepository.createUser("user1") }
-                val tags = transaction { tagRepository.createTags(listOf("tag1", "tag2", "tag3")) }
-
-                val knowhow = transaction {
-                    knowhowRepository.addKnowhow(
-                        "Knowhow sample",
-                        "dummy URL",
-                        author.id,
-                        tags.map { it.id }
-                    )
+        describe("knowhow") {
+            it("not exist") {
+                transaction {
+                    knowhowRepository.getAll().shouldBeEmpty()
                 }
+            }
+            it("add") {
+                it("success") {
+                    val knowhow = transaction {
+                        knowhowRepository.addKnowhow(
+                            "Knowhow sample",
+                            "dummy URL",
+                            author.id,
+                            tags.map { it.id }
+                        )
+                    }
 
-                knowhow.let {
-                    it.title shouldBe "Knowhow sample"
-                    it.author.name shouldBe author.name
-                    it.tags.size shouldBe 3
+                    knowhow.let {
+                        it.title shouldBe "Knowhow sample"
+                        it.author.name shouldBe author.name
+                        it.tags.size shouldBe 3
+                    }
+                }
+                it("failure") {
+                    it("when user does not exist") {
+                        transaction {
+                            shouldThrow<EntityNotFoundException> {
+                                knowhowRepository.addKnowhow(
+                                    "Knowhow sample",
+                                    "dummy URL",
+                                    2L,
+                                    tags.map { it.id }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            it("exist") {
+                transaction {
+                    knowhowRepository.getAll().size shouldBe 1
                 }
             }
         }
